4e054d970047e759dd0c8c06f3efd79e04ad88a7
haha <390519764@qq.com>
2025-11-13 23:19:23 +0800

    refactor: 增加容错处理

--- src/utils/request.ts
+++ src/utils/request.ts
@@ -1,8 +1,22 @@
-import axios from "axios";
+import axios from 'axios';
 
 export const request = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
   headers: {
-    'Authorization': `Bearer ${import.meta.env.VITE_API_KEY}`,
+    Authorization: `Bearer ${import.meta.env.VITE_API_KEY}`,
   },
 });
+
+request.interceptors.response.use(
+  response => {
+    let data = response.data;
+    // 防止object变成array
+    if (Array.isArray(data)) {
+      data = data[0];
+    }
+    return { ...response, data };
+  },
+  error => {
+    return Promise.reject(error);
+  }
+);
